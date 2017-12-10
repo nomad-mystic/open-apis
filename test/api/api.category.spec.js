0fb3842b5a32627f087027cf73e8db42dccbc18d
nomad-mystic <nomadmystics@gmail.com>
2017-12-09 18:14:11 -0800

    tests: Building out base API tests for category endpoint.

--- test/api/api.category.spec.js
+++ test/api/api.category.spec.js
@@ -87,6 +87,326 @@ describe('This is for the category endpoints', function() {
 				});
 		});
 	});
+	describe('Security endpoint', function() {
+		it('Should have a status of 200', function(done) {
+			chai.request(server)
+				.get('/api/category/Security')
+				.end(function(err, res) {
+					expect(res).to.have.status(200);
+					done();
+				});
+		});
+		it('Should have no errors', function(done) {
+			chai.request(server)
+				.get('/api/category/Security')
+				.end(function(err, res) {
+					expect(err).to.be.null;
+					done();
+				});
+
+		});
+		it('Should return JSON', function(done) {
+			chai.request(server)
+				.get('/api/category/Security')
+				.end(function(err, res) {
+					expect(res).to.be.json;
+					done();
+				});
+
+		});
+		it('Should have headers', function(done) {
+			chai.request(server)
+				.get('/api/category/Security')
+				.end(function(err, res) {
+					expect(res).to.have.headers;
+					done();
+				});
+		});
+	});
+	describe('Shopping endpoint', function() {
+		it('Should have a status of 200', function(done) {
+			chai.request(server)
+				.get('/api/category/Shopping')
+				.end(function(err, res) {
+					expect(res).to.have.status(200);
+					done();
+				});
+		});
+		it('Should have no errors', function(done) {
+			chai.request(server)
+				.get('/api/category/Shopping')
+				.end(function(err, res) {
+					expect(err).to.be.null;
+					done();
+				});
+
+		});
+		it('Should return JSON', function(done) {
+			chai.request(server)
+				.get('/api/category/Shopping')
+				.end(function(err, res) {
+					expect(res).to.be.json;
+					done();
+				});
+
+		});
+		it('Should have headers', function(done) {
+			chai.request(server)
+				.get('/api/category/Shopping')
+				.end(function(err, res) {
+					expect(res).to.have.headers;
+					done();
+				});
+		});
+	});
+	describe('Social endpoint', function() {
+		it('Should have a status of 200', function(done) {
+			chai.request(server)
+				.get('/api/category/Social')
+				.end(function(err, res) {
+					expect(res).to.have.status(200);
+					done();
+				});
+		});
+		it('Should have no errors', function(done) {
+			chai.request(server)
+				.get('/api/category/Social')
+				.end(function(err, res) {
+					expect(err).to.be.null;
+					done();
+				});
+
+		});
+		it('Should return JSON', function(done) {
+			chai.request(server)
+				.get('/api/category/Social')
+				.end(function(err, res) {
+					expect(res).to.be.json;
+					done();
+				});
+
+		});
+		it('Should have headers', function(done) {
+			chai.request(server)
+				.get('/api/category/Social')
+				.end(function(err, res) {
+					expect(res).to.have.headers;
+					done();
+				});
+		});
+	});
+	describe('Sports & Fitness endpoint', function() {
+		it('Should have a status of 200', function(done) {
+			chai.request(server)
+				.get('/api/category/Sports & Fitness')
+				.end(function(err, res) {
+					expect(res).to.have.status(200);
+					done();
+				});
+		});
+		it('Should have no errors', function(done) {
+			chai.request(server)
+				.get('/api/category/Sports & Fitness')
+				.end(function(err, res) {
+					expect(err).to.be.null;
+					done();
+				});
+
+		});
+		it('Should return JSON', function(done) {
+			chai.request(server)
+				.get('/api/category/Sports & Fitness')
+				.end(function(err, res) {
+					expect(res).to.be.json;
+					done();
+				});
+
+		});
+		it('Should have headers', function(done) {
+			chai.request(server)
+				.get('/api/category/Sports & Fitness')
+				.end(function(err, res) {
+					expect(res).to.have.headers;
+					done();
+				});
+		});
+	});
+	describe('Test Data endpoint', function() {
+		it('Should have a status of 200', function(done) {
+			chai.request(server)
+				.get('/api/category/Test Data')
+				.end(function(err, res) {
+					expect(res).to.have.status(200);
+					done();
+				});
+		});
+		it('Should have no errors', function(done) {
+			chai.request(server)
+				.get('/api/category/Test Data')
+				.end(function(err, res) {
+					expect(err).to.be.null;
+					done();
+				});
+
+		});
+		it('Should return JSON', function(done) {
+			chai.request(server)
+				.get('/api/category/Test Data')
+				.end(function(err, res) {
+					expect(res).to.be.json;
+					done();
+				});
+
+		});
+		it('Should have headers', function(done) {
+			chai.request(server)
+				.get('/api/category/Test Data')
+				.end(function(err, res) {
+					expect(res).to.have.headers;
+					done();
+				});
+		});
+	});
+	describe('Text Analysis endpoint', function() {
+		it('Should have a status of 200', function(done) {
+			chai.request(server)
+				.get('/api/category/Text Analysis')
+				.end(function(err, res) {
+					expect(res).to.have.status(200);
+					done();
+				});
+		});
+		it('Should have no errors', function(done) {
+			chai.request(server)
+				.get('/api/category/Text Analysis')
+				.end(function(err, res) {
+					expect(err).to.be.null;
+					done();
+				});
+
+		});
+		it('Should return JSON', function(done) {
+			chai.request(server)
+				.get('/api/category/Text Analysis')
+				.end(function(err, res) {
+					expect(res).to.be.json;
+					done();
+				});
+
+		});
+		it('Should have headers', function(done) {
+			chai.request(server)
+				.get('/api/category/Text Analysis')
+				.end(function(err, res) {
+					expect(res).to.have.headers;
+					done();
+				});
+		});
+	});
+	describe('Tracking endpoint', function() {
+		it('Should have a status of 200', function(done) {
+			chai.request(server)
+				.get('/api/category/Tracking')
+				.end(function(err, res) {
+					expect(res).to.have.status(200);
+					done();
+				});
+		});
+		it('Should have no errors', function(done) {
+			chai.request(server)
+				.get('/api/category/Tracking')
+				.end(function(err, res) {
+					expect(err).to.be.null;
+					done();
+				});
+
+		});
+		it('Should return JSON', function(done) {
+			chai.request(server)
+				.get('/api/category/Tracking')
+				.end(function(err, res) {
+					expect(res).to.be.json;
+					done();
+				});
+
+		});
+		it('Should have headers', function(done) {
+			chai.request(server)
+				.get('/api/category/Tracking')
+				.end(function(err, res) {
+					expect(res).to.have.headers;
+					done();
+				});
+		});
+	});
+	describe('Transportation endpoint', function() {
+		it('Should have a status of 200', function(done) {
+			chai.request(server)
+				.get('/api/category/Transportation')
+				.end(function(err, res) {
+					expect(res).to.have.status(200);
+					done();
+				});
+		});
+		it('Should have no errors', function(done) {
+			chai.request(server)
+				.get('/api/category/Transportation')
+				.end(function(err, res) {
+					expect(err).to.be.null;
+					done();
+				});
+		});
+		it('Should return JSON', function(done) {
+			chai.request(server)
+				.get('/api/category/Transportation')
+				.end(function(err, res) {
+					expect(res).to.be.json;
+					done();
+				});
+		});
+		it('Should have headers', function(done) {
+			chai.request(server)
+				.get('/api/category/Transportation')
+				.end(function(err, res) {
+					expect(res).to.have.headers;
+					done();
+				});
+		});
+	});
+	describe('University endpoint', function() {
+		it('Should have a status of 200', function(done) {
+			chai.request(server)
+				.get('/api/category/University')
+				.end(function(err, res) {
+					expect(res).to.have.status(200);
+					done();
+				});
+		});
+		it('Should have no errors', function(done) {
+			chai.request(server)
+				.get('/api/category/University')
+				.end(function(err, res) {
+					expect(err).to.be.null;
+					done();
+				});
+		});
+		it('Should return JSON', function(done) {
+			chai.request(server)
+				.get('/api/category/University')
+				.end(function(err, res) {
+					expect(res).to.be.json;
+					done();
+				});
+		});
+		it('Should have headers', function(done) {
+			chai.request(server)
+				.get('/api/category/University')
+				.end(function(err, res) {
+					expect(res).to.have.headers;
+					done();
+				});
+		});
+	});
 	describe('URL Shorteners endpoint', function() {
 		it('Should have a status of 200', function(done) {
 			chai.request(server)
@@ -115,6 +435,108 @@ describe('This is for the category endpoints', function() {
 		it('Should have headers', function(done) {
 			chai.request(server)
 				.get('/api/category/URL Shorteners')
+				.end(function(err, res) {
+					expect(res).to.have.headers;
+					done();
+				});
+		});
+	});
+	describe('Vehicle endpoint', function() {
+		it('Should have a status of 200', function(done) {
+			chai.request(server)
+				.get('/api/category/Vehicle')
+				.end(function(err, res) {
+					expect(res).to.have.status(200);
+					done();
+				});
+		});
+		it('Should have no errors', function(done) {
+			chai.request(server)
+				.get('/api/category/Vehicle')
+				.end(function(err, res) {
+					expect(err).to.be.null;
+					done();
+				});
+		});
+		it('Should return JSON', function(done) {
+			chai.request(server)
+				.get('/api/category/Vehicle')
+				.end(function(err, res) {
+					expect(res).to.be.json;
+					done();
+				});
+		});
+		it('Should have headers', function(done) {
+			chai.request(server)
+				.get('/api/category/Vehicle')
+				.end(function(err, res) {
+					expect(res).to.have.headers;
+					done();
+				});
+		});
+	});
+	describe('Video endpoint', function() {
+		it('Should have a status of 200', function(done) {
+			chai.request(server)
+				.get('/api/category/Video')
+				.end(function(err, res) {
+					expect(res).to.have.status(200);
+					done();
+				});
+		});
+		it('Should have no errors', function(done) {
+			chai.request(server)
+				.get('/api/category/Video')
+				.end(function(err, res) {
+					expect(err).to.be.null;
+					done();
+				});
+		});
+		it('Should return JSON', function(done) {
+			chai.request(server)
+				.get('/api/category/Video')
+				.end(function(err, res) {
+					expect(res).to.be.json;
+					done();
+				});
+		});
+		it('Should have headers', function(done) {
+			chai.request(server)
+				.get('/api/category/Video')
+				.end(function(err, res) {
+					expect(res).to.have.headers;
+					done();
+				});
+		});
+	});
+	describe('Weather endpoint', function() {
+		it('Should have a status of 200', function(done) {
+			chai.request(server)
+				.get('/api/category/Weather')
+				.end(function(err, res) {
+					expect(res).to.have.status(200);
+					done();
+				});
+		});
+		it('Should have no errors', function(done) {
+			chai.request(server)
+				.get('/api/category/Weather')
+				.end(function(err, res) {
+					expect(err).to.be.null;
+					done();
+				});
+		});
+		it('Should return JSON', function(done) {
+			chai.request(server)
+				.get('/api/category/Weather')
+				.end(function(err, res) {
+					expect(res).to.be.json;
+					done();
+				});
+		});
+		it('Should have headers', function(done) {
+			chai.request(server)
+				.get('/api/category/Weather')
 				.end(function(err, res) {
 					expect(res).to.have.headers;
 					done();
